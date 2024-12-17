548e97360644b6fa574ca4ce78593642feb33b97
Donald Langston <donald_langston@yahoo.com>
2024-12-17 08:02:15 -0500

    ci: add playwright tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { AuthContextProvider } from './contexts/AuthContext.jsx'
+import PropTypes from 'prop-types'
+import { HelmetProvider } from 'react-helmet-async'
 
-function App() {
-  const [count, setCount] = useState(0)
+const queryClient = new QueryClient()
 
+export function App({ children }) {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthContextProvider>{children}</AuthContextProvider>
+      </QueryClientProvider>
+    </HelmetProvider>
   )
 }
 
-export default App
+App.propTypes = {
+  children: PropTypes.element.isRequired,
+}
---
+++ src/entry-server.jsx
@@ -0,0 +1,23 @@
+import ReactDomServer from 'react-dom/server'
+import {
+  createStaticHandler,
+  createStaticRouter,
+  StaticRouterProvider,
+} from 'react-router-dom/server'
+import { App } from './App.jsx'
+import { routes } from './routes.jsx'
+import { createFetchRequest } from './request.js'
+
+const handler = createStaticHandler(routes)
+
+export async function render(req) {
+  const fetchRequest = createFetchRequest(req)
+  const context = await handler.query(fetchRequest)
+  const router = createStaticRouter(handler.dataRoutes, context)
+
+  return ReactDomServer.renderToString(
+    <App>
+      <StaticRouterProvider router={router} context={context} />
+    </App>,
+  )
+}
