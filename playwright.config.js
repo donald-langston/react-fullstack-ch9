06494edc7fa5415cb2e3bb8e0799fd4ed95135d2
Donald Langston <donald_langston@yahoo.com>
2024-12-18 08:03:36 -0500

    ci: changed base url  in playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -72,8 +72,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run e2e',
-    url: 'http://localhost:5173',
+    url: 'http://127.0.0.1:5173',
     //   reuseExistingServer: !process.env.CI,
   },
-  timeout: 150_000,
 })
