4c0fb3cb55392e62f65ca61559def453eab60f54
Donald Langston <donald_langston@yahoo.com>
2024-12-20 10:57:29 -0500

    ci: testing changes to playwright.yml

--- tests/fixtures/AuthFixture.js
+++ tests/fixtures/AuthFixture.js
@@ -10,13 +10,13 @@ export class AuthFixture {
     await this.page.getByLabel('Username:').fill(testUser)
     await this.page.getByLabel('Password:').click()
     await this.page.getByLabel('Password:').fill('password')
-    await this.page.getByRole('button', { name: 'Sign Up' }).click()
+    await this.page.getByRole('button', { name: 'Sign Up' }).press('Enter')
     await this.page.waitForURL('**/login')
     await this.page.getByLabel('Username:').click()
     await this.page.getByLabel('Username:').fill(testUser)
     await this.page.getByLabel('Password:').click()
     await this.page.getByLabel('Password:').fill('password')
-    await this.page.getByRole('button', { name: 'Log In' }).click()
+    await this.page.getByRole('button', { name: 'Log In' }).press('Enter')
     await this.page.waitForURL('**/')
     return testUser
   }
